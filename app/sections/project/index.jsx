4832729ab3f1ad14435847a03a3278582cb3cd5d
Hussain101 <hussainsidd88@gmail.com>
2024-05-20 14:25:50 +0500

    updated with project section

--- app/sections/project/index.jsx
+++ app/sections/project/index.jsx
@@ -1,61 +1,54 @@
-import { Suspense, useRef } from "react";
+import { HeadingDivider } from "components";
 import { domAnimation, LazyMotion, useInView } from "framer-motion";
-import Link from "next/link";
-import useSWR from "swr";
-import { HeadingDivider, Loader } from "components";
-import { fetcher } from "utils/fetcher";
-import Error from "../../error";
-import { ErrorBoundary } from "react-error-boundary";
-import { Projects } from "../../projects/components/Projects";
-import { SITE_ROUTES } from "../../../constants";
-
-const url = `${process.env.NEXT_PUBLIC_SANITY_URL}${process.env.NEXT_PUBLIC_SANITY_LATEST_PROJECTS}`;
-
+import Image from "next/image";
+import { BsLink } from "react-icons/bs";
 export function ProjectsSection() {
-	const btnRef = useRef(null);
-	const isBtnInView = useInView(btnRef, { once: true });
+    const projects = [
+		{
+			id:1,
+			image:<Image src={"/assets/images/italipino.png"} width={300} height={150} />,
+			name:"ITALIPINO",
+			link:"https://italipino-phi.vercel.app/",
+			desc:"Platform to connect Gardeners worldwide."
 
-	const { data, error } = useSWR(url, fetcher);
-	const projects = data?.result;
+		},
+		{
+			id:2,
+			image:<Image src={"/assets/images/hockey.png"} width={300} height={150} />,
+			name:"THE HOCKEY REVIEW",
+			link:"https://www.thehockeyreview.com/",
+			desc:"Hockey review management system with admin dashboard"
 
-	if (error && !data) {
-		return null;
-	}
 
+		},
+		{
+			id:3,
+			image:<Image src={"/assets/images/gem-hunter.png"} width={300} height={150} />,
+			name:"GEM HUNTERS",
+			link:"https://gem-hunters-git-signup-russ-the-man.vercel.app/",
+			desc:"NFT based website and payment method integrated with coinremitter"
+
+		},
+	]
 	return (
 		<LazyMotion features={domAnimation}>
 			<section id="projects" className="section">
-				<HeadingDivider title="Latest projects" />
-				<div className="h-10 md:h-14" />
-
-				<div className="flex flex-col items-center gap-8 md:gap-14">
-					<Suspense
-						fallback={
-							<div className="flex-center">
-								<Loader />
+			<HeadingDivider title="PROJECTS" />
+			<div className="flex gap-6 mt-10 justify-center">
+				{
+					projects.map((item) =>{
+						return(
+							<div key={item.id} className="flex flex-col gap-2 items-center">
+							<div className="rounded-t-lg w-[300px] h-[150px] bg-white overflow-hidden">
+								{item.image}
 							</div>
-						}
-					>
-						<ErrorBoundary FallbackComponent={Error}>
-							<Projects projects={projects} />
-						</ErrorBoundary>
-					</Suspense>
-
-					<Link
-						href={SITE_ROUTES.projects}
-						tabIndex={-1}
-						aria-label="Go to projects page"
-						ref={btnRef}
-						className="btn"
-						style={{
-							transform: btnRef ? "none" : "translateX(-50px)",
-							opacity: isBtnInView ? 1 : 0,
-							transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-						}}
-					>
-						<button aria-label="See more projects">More projects</button>
-					</Link>
-				</div>
+							<p className="text-lg font-semibold mt-2 text-center flex items-center justify-center gap-1">{item.name} <BsLink /> </p>
+							<p className="w-[250px] text-center">{item.desc}</p>
+						</div>
+						)
+					})
+				}
+			</div>
 			</section>
 		</LazyMotion>
 	);
